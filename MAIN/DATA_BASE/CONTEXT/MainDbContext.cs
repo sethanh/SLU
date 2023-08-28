6b4f117d572555a504d0032fcbf652039233fdb2
thanhse <thanhse1998@gmail.com>
2023-08-28 08:14:10 +0700

    first commit

---
+++ MAIN/DATA_BASE/CONTEXT/MainDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DATA_BASE.EF_CORE;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DATA_BASE.CONTEXT
+{
+    public class MainDbContext : DbContext
+    {
+        public DbSet<UserGroup> UserGroups { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public MainDbContext(DbContextOptions<MainDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
---
+++ MAIN/DATA_BASE/EF_CORE/UserGroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DATA_BASE.EF_CORE
+{
+    public class UserGroup
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreationDateTime { get; set; }
+        public DateTime? LastUpdateDateTime { get; set; }
+
+        public ICollection<User> Users { get; set; }
+    }
+}
